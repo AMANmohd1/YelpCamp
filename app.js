4d5e373bbce39097708bb4c22a4ea064948e4d8e
AMANmohd1 <mohdaman2001@gmail.com>
2022-01-12 21:38:06 +0530

    Basic styling using bootstrap, partials and ejs-mate

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const { handle } = require('express/lib/application');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -15,6 +16,7 @@ db.once("open", ()=>{
     console.log("Database Connected");
 })
 
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i = 0; i< 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 1
         const camp = new Campground({
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit aut error maxime optio sed! Quam aut debitis, cumque similique sequi deleniti error provident, sit quod maxime qui! Dolorem, sequi provident.',
+            price
         });
         await camp.save();
     }
@@ -31,3 +35,5 @@ const seedDB = async() =>{
 seedDB().then(() =>{
     mongoose.connection.close();
 })
+
+
