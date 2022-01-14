398546bc720c6efd8d2421ac649e7fdf8c59e31d
AMANmohd1 <mohdaman2001@gmail.com>
2022-01-14 14:18:34 +0530

    Error handling using Joi and inbuilt express error handles

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const { handle } = require('express/lib/application');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const { nextTick } = require('process');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const {campgroundSchema} = require('./schema.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -23,6 +27,18 @@ app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req,res,next) =>{
+    
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400);
+    }else{
+        next();
+    }
+}
+
+
 app.get('/',(req,res)=>{
     res.render('home');
 })
@@ -36,16 +52,18 @@ app.get('/campgrounds/new', (req,res) =>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',async(req,res) =>{
+app.post('/campgrounds',catchAsync(async(req,res,next) =>{
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
-app.get('/campgrounds/:id/edit', async(req,res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit',{campground});
-})
+}))
 
 app.put('/campgrounds/:id', async(req,res)=>{
     const {id} = req.params;
@@ -64,6 +82,15 @@ app.delete('/campgrounds/:id',async(req,res) =>{
     res.redirect('/campgrounds');
 })
 
+app.all('*',(req,res,next) =>{
+    next(new ExpressError('Page not found',404))
+})
+
+app.use((err,req,res,next) =>{
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh NO Something went wrong.'
+    res.status(statusCode).render('error',{err})
+})
 
 
 
